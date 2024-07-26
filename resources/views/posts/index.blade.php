b7a446cfa0b2632c676c68f0e8a6826176e4f90a
gab <corbanigabriele@gmail.com>
2024-07-26 17:06:56 +0200

    ADD: post index

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+@section('content')
+
+<h1 class="text-center">POSTS</h1>
+
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">nome</th>
+        <th scope="col">slug</th>
+        <th scope="col">read time</th>
+        <th scope="col">difficulty</th>
+        <th scope="col">featured</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach($posts as $post)
+        <tr>
+
+            <th scope="row">{{$post->id}}</th>
+            <td>{{$post->name}}</td>
+            <td>{{$post->slug}}</td>
+            <td>{{$post->read_time}}</td>
+            <td>{{$post->difficulty}}</td>
+            <td>{{$post->featured}}</td>
+            <td>{{$post->created_at}}</td>
+        </tr>
+        @endforeach
+    </tbody>
+
+@endsection
