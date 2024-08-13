408df8586b050f9df0fa428204700f2deb8d5a8d
gab <corbanigabriele@gmail.com>
2024-08-13 17:53:40 +0200

    REFACT: show post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,23 +8,23 @@
     <thead>
         <tr>
             <th scope="col">#</th>
-            {{-- <th scope="col">nome</th> --}}
-            <th scope="col">content</th>
-            <th scope="col">creator</th>
+            <th scope="col">title</th>
+            {{-- <th scope="col">content</th> --}}
+            {{-- <th scope="col">creator</th> --}}
             <th scope="col">read time</th>
             <th scope="col">featured</th>
             <th scope="col">difficulty</th>
-            <th scope="col">created</th>
             <th scope="col">tags</th>
             <th scope="col">technologies</th>
+            <th scope="col">created</th>
         </tr>
     </thead>
     <tbody>
         <tr>
             <th scope="row">{{$post->id}}</th>
-            {{-- <td>{{$post->title}}</td> --}}
-            <td>{{$post->content}}</td>
-            <td>{{$post->user->name}}</td>
+            <td>{{$post->title}}</td>
+            {{-- <td>{{$post->content}}</td> --}}
+            {{-- <td>{{$post->user->name}}</td> --}}
             <td>{{ $post->reading_time }}</td>
             <td>
                 @if($post->featured == 1)
@@ -32,13 +32,12 @@
                 @elseif($post->featured == 0)
                 False
                 @endif
-              </td>
-              <td>
+            </td>
+            <td>
                 @for($i = 0; $i < $post->difficulty; $i++)
                 <i class="bi bi-star-fill"></i>
                 @endfor
-              </td>
-            <td>{{$post->created_at->translatedFormat('d F Y ')}}</td>
+            </td>
             <td>
                 @foreach($post->tags as $tag)
                 {{$tag->name}}
@@ -49,6 +48,7 @@
                 {{$technology->name}}
                 @endforeach
             </td>
+            <td>{{$post->created_at->translatedFormat('d F Y ')}}</td>
         </tr>
     </tbody>
 </table>
