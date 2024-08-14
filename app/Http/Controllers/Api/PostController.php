67a30968f13c0b4961ef3a09466bf3b2766d2354
gab <corbanigabriele@gmail.com>
2024-08-14 15:25:45 +0200

    ADD: api basic post

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Recuperare i post
+        $posts = Post::all(); // Impaginazione
+
+        // Logice aggiuntive per ricerca
+
+
+        return response()->json($posts);
+
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return response()->json($post);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
