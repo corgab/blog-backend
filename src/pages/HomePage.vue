abed0c3238b6f12576b2b129217666ce9ae4d63d
gab <corbanigabriele@gmail.com>
2024-09-13 01:24:23 +0200

    REFACT: scaffolding ADD: card style

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <!-- <CardContent v-if="posts" v-for="post in posts" :key="post.id" :post="post"/> -->
+        <!-- <h2 v-else>Nessun Post...</h2> -->
+         <CardHighLights/>
+    </div>
+</template>
+
+<script>
+// import CardContent from "../components/CardContent.vue";
+import CardHighLights from '../components/PageComponents/HighLights.vue'
+
+export default {
+    components: {
+        CardHighLights
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
