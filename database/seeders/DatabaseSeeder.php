1c9d6704d130e827795a173868813be15388565e
gab <corbanigabriele@gmail.com>
2024-09-13 02:07:02 +0200

    ADD: imgs seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             TagSeeder::class,
             TechnologySeeder::class,
             PostSeeder::class,
+            ImageSeeder::class
          ]);
     }
 }
---
+++ database/seeders/ImageSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Image;
+
+class ImageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $imagePath = 'images/927d6381-885e-4e1c-8c1f-fc575907cfa9.webp';
+
+        for($i = 1; $i < 100; $i++) {
+            $newImage = New Image();
+
+            $newImage->path = $imagePath;
+            $newImage->post_id = $i;
+            $newImage->is_featured = 1;
+
+            $newImage->save();
+        }
+    }
+}
