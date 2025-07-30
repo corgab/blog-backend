793b6965c85b09846274560b26bfa4aa46cc3445
corgab <corbanigabriele@gmail.com>
2025-07-30 15:16:45 +0200

    fix date in italiano

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Post;
 use App\Observers\PostObserver;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Carbon::setLocale('it');
         Post::observe(PostObserver::class);
     }
 }
